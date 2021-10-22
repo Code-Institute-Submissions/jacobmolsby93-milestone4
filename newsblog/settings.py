1c5c8fe4495f1855eaf8f4e3b5798044059be9cb
Jacob Molsby <jacobmolsby@hotmail.se>
2021-10-22 19:59:27 +0000

    deployment 1

--- newsblog/settings.py
+++ newsblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['milestone4.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['milestone-news-4.herokuapp.com', 'localhost']
 
 
 # Application definition
